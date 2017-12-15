a8e0e54002d7af127ca2140bf4fd7e4e8b7774eb
Jyotish <hypothesis1996@gmail.com>
2017-12-16 04:33:12 +0530

    Fix remover.sh

--- guest/remover.sh
+++ guest/remover.sh
@@ -6,12 +6,12 @@ base_dn="ou=Guest,ou=Users,dc=iiit,dc=ac,dc=in"
 ldap_host="ldap://ldap.iiit.ac.in"
 to="someone@something"
 
-expired_users=$(ldapsearch -H $ldap_host -x -b "$base_dn" "sambaSID>=`date +%s`" uid | grep uid: | awk '{print $2}')
+expired_users=$(ldapsearch -H $ldap_host -x -b "$base_dn" "sambaSID<=`date +%s`" uid | grep uid: | awk '{print $2}')
 
 for user in $expired_users
 do
-  email=ldapsearch -H $ldap_host -x -b "$base_dn" "uid=$user"
-  if ldapdelete -H ldap://ldap.iiit.ac.in -x "uid=$user,$base_din" -D $admin_dn -w $admin_password >/dev/null 2>&1; then
+  email=`ldapsearch -H $ldap_host -x -b "$base_dn" "uid=$user"`
+  if `ldapdelete -H ldap://ldap.iiit.ac.in -x "uid=$user,$base_din" -D $admin_dn -w $admin_password >/dev/null 2>&1`; then
     logger --prio-prefix "Guest remover" $email
   else
     error=$(ldapdelete -H ldap://ldap.iiit.ac.in -x "uid=$user,$base_din" -D $admin_dn -w $admin_password 2>&1)
